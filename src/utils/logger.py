2536148f0c43b2f0c8111aad3775563f536b4c0e
Bui Huu Tai <huutaiii2410@gmail.com>
2026-03-17 22:44:47 +0700

    add stream logger and remove pgadmin from docker

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,4 +1,5 @@
 import logging
+import sys
 import os
 from pathlib import Path
 
@@ -14,16 +15,25 @@ def get_logger(name : str) -> logging.Logger:
     # Create logger based on module's name
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
+
     # Config logger if not already exists
     if not logger.handlers:
+
         # create handler object
-        handler = logging.FileHandler(LOG_FILE, mode = "a")
+        file_handler = logging.FileHandler(LOG_FILE, mode = "a")
+        stream_handler = logging.StreamHandler(sys.stdout)
+
         # create formatter object
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - [%(filename)s:%(lineno)d] - %(message)s')
+
         # set format for the handler
-        handler.setFormatter(formatter)
+        file_handler.setFormatter(formatter)
+        stream_handler.setFormatter(formatter)
+
         # set hanlder for the logger
-        logger.addHandler(handler)
+        logger.addHandler(file_handler)
+        logger.addHandler(stream_handler)
+        
     # return logger
     return logger
 
